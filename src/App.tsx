2c89ce1d7094e742dfdb350622aa7bfe87885f34
Matheus Ribeiro Menezes <matheusmenezzes10@gmail.com>
2024-11-11 20:48:28 -0300

    adiciona props

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import Disciplina from "./Disciplina";
 
 export default function App() {
-  return(
-  <>
-    <h1>Olá Mundo!</h1>
-    <Disciplina />
-  </>
-  )
+  return (
+    <>
+      <h1>Olá Mundo!</h1>
+      <Disciplina codigo="DCC202" nome="Desenvolvimento Web I" />
+      <Disciplina codigo="DCC206" nome="Desenvolvimento Web II" />
+      <Disciplina codigo="DCC207" nome="Desenvolvimento Web Front-End" />
+    </>
+  );
 }
--- src/Disciplina.tsx
+++ src/Disciplina.tsx
@@ -1,13 +1,16 @@
 import "./Disciplina.css";
 
-export default function Disciplina() {
-    const codigo = "DCC207";
-    const nome = "Desenvolvimento Web Front-End";
+type DisciplinaProps = {
+    codigo: string;
+    nome: string;
+}
 
+export default function Disciplina(props: DisciplinaProps) {
+    
   return (
   <div className="disciplina">
-    <div>{codigo}</div>
-    <div>{nome}</div>
+    <div>{props.codigo}</div>
+    <div>{props.nome}</div>
   </div>
   );
 }
